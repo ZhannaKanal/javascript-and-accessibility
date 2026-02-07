a081f839c2a59bb2ac1c9b2f98cb789d81e6ed72
Zhanna <kezhanna.03@mail.ru>
2026-02-08 02:33:15 +0500

    task 23

--- planetsTablist/script.js
+++ planetsTablist/script.js
@@ -3,10 +3,10 @@ const panels = document.querySelectorAll('[role="tabpanel"]');
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     tabs.forEach((t) => t.setAttribute("aria-selected", "false"));
-      panels.forEach((p) => (p.hidden = true));
-      tab.setAttribute("aria-selected", "true");
-      const associatedPanel = tab.getAttribute("aria-controls");
-      const panel = document.getElementById(associatedPanel);
-
+    panels.forEach((p) => (p.hidden = true));
+    tab.setAttribute("aria-selected", "true");
+    const associatedPanel = tab.getAttribute("aria-controls");
+    const panel = document.getElementById(associatedPanel);
+    panel.hidden = false;
   });
 });
