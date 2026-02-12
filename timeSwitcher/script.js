8ea7a10e935d0d172e9aafc7cce3428aad93f871
Zhanna <kezhanna.03@mail.ru>
2026-02-13 03:14:10 +0500

    task 25

--- timeSwitcher/script.js
+++ timeSwitcher/script.js
@@ -6,6 +6,8 @@ const themes = [
   { name: "ocean", message: "Ocean msg" },
   { name: "nord", message: "Nord msg" },
 ];
+const themeItems = document.querySelectorAll("li");
+const liveRegion = document.querySelector('[aria-live="polite"]');
 themeSwitcherButton.addEventListener("click", () => {
   dropdown.hidden = !dropdown.hidden;
   themeSwitcherButton.setAttribute("aria-expanded", !dropdown.hidden);
@@ -16,6 +18,9 @@ themeItems.forEach((item) => {
     const themeName = item.id.replace("theme-", "");
     document.body.className = "";
     document.body.classList.add(`theme-${themeName}`);
+
+    const selectedTheme = themes.find((theme) => theme.name === themeName);
+    liveRegion.textContent = selectedTheme.message;
 
     dropdown.hidden = true;
     themeSwitcherButton.setAttribute("aria-expanded", "false");
